13053f6d86ed4c1183e43123d8091957a83d7c50
Roopa <39237061+Roopa-palani-samy@users.noreply.github.com>
2019-05-10 16:41:02 +0530

    Add files via upload
    Solution  for problem 1nd 2

---
+++ boto1.py
@@ -0,0 +1,36 @@
+import boto3
+import datetime
+import time
+# Solution for the 1 st and 2nd one 
+# Create IAM client
+iam = boto3.client('iam')
+
+username = input('enter username to be verified')
+res = iam.list_access_keys(UserName=username)
+print(res)
+accesskeydate=res['AccessKeyMetadata'][0]['CreateDate']
+accesskeydate = accesskeydate.strftime("%Y-%m-%d %H:%M:%S")
+currentdate = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+accesskeyd = time.mktime(datetime.datetime.strptime(accesskeydate, "%Y-%m-%d %H:%M:%S").timetuple())
+currentd = time.mktime(datetime.datetime.strptime(currentdate, "%Y-%m-%d %H:%M:%S").timetuple())
+
+active_days = (currentd - accesskeyd)/60/60/24 ### We get the data in seconds. converting it to days
+print (int(round(active_days)))
+
+if active_days>=100:
+    response = access_key.deactivate()
+else :
+    print("Access key is activated")
+
+
+
+
+
+
+# Get last use of access key
+# response = iam.get_access_key_last_used(
+#     AccessKeyId='AKIAQAQEAS4WFUGWGEGF'
+# )
+
+# print(response['AccessKeyLastUsed'])
