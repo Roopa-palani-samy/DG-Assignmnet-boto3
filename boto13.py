6040067c7832ef7217e432feeb622e7b87351a96
Roopa <39237061+Roopa-palani-samy@users.noreply.github.com>
2019-05-10 16:43:56 +0530

    Add files via upload
    Solution for problem 3

---
+++ boto13.py
@@ -0,0 +1,30 @@
+import boto3
+
+client = boto3.client('ec2')
+print(client)
+# Tagging owner id because user name is tagged by default 
+response = client.describe_instances(
+    Filters=[
+        {
+            'Name': 'owner-id',
+            'Values': [
+                '001082169132',
+            ]
+        },
+    ]
+)
+
+owner_id = '001082169132'
+res = client.create_tags(
+    
+    Resources=[
+        'i-0efb5fdb52b2ff78b',
+    ],
+    Tags=[
+        {
+            'Key': 'ownername',
+            'Value': owner_id
+        },
+    ]
+)
+print(res)
